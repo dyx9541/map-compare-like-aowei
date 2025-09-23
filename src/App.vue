4991ae165f9abd08e78b72069c015c724daa29b7
dyx9541 <43093521+dyx9541@users.noreply.github.com>
2025-09-23 21:15:36 +0800

    feat: synchronize map interactions

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-layout">
+    <div class="map-column">
+      <div ref="leftMap" class="map"></div>
+    </div>
+    <div class="map-column">
+      <div ref="rightMap" class="map"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+import { fromLonLat } from 'ol/proj'
+import 'ol/ol.css'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const leftMap = ref(null)
+    const rightMap = ref(null)
+
+    const view = new View({
+      center: fromLonLat([0, 0]),
+      zoom: 2
+    })
+
+    const createMap = (target) => {
+      if (!target) {
+        return null
+      }
+
+      return new Map({
+        target,
+        layers: [
+          new TileLayer({
+            source: new OSM()
+          })
+        ],
+        view
+      })
+    }
+
+    onMounted(() => {
+      createMap(leftMap.value)
+      createMap(rightMap.value)
+    })
+
+    return {
+      leftMap,
+      rightMap
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+
+.app-layout {
+  display: flex;
+  height: 100vh;
+}
+
+.map-column {
+  flex: 1;
+  min-width: 0;
+}
+
+.map {
+  width: 100%;
+  height: 100%;
 }
 </style>
