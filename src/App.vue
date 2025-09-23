b655b925b1993e592e6a8c5e3f4fdd27cf0b2f3b
dyx9541 <43093521+dyx9541@users.noreply.github.com>
2025-09-23 17:37:09 +0800

    feat: add side-by-side openlayers maps

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-layout">
+    <div class="map-column">
+      <div ref="leftMap" class="map"></div>
+    </div>
+    <div class="map-column">
+      <div ref="rightMap" class="map"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* global ol */
+import { onMounted, ref } from 'vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const leftMap = ref(null)
+    const rightMap = ref(null)
+
+    const createMap = (target) => {
+      if (!target || typeof ol === 'undefined') {
+        return null
+      }
+
+      return new ol.Map({
+        target,
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([0, 0]),
+          zoom: 2
+        })
+      })
+    }
+
+    onMounted(() => {
+      createMap(leftMap.value)
+      createMap(rightMap.value)
+    })
+
+    return {
+      leftMap,
+      rightMap
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+
+.app-layout {
+  display: flex;
+  height: 100vh;
+}
+
+.map-column {
+  flex: 1;
+  min-width: 0;
+}
+
+.map {
+  width: 100%;
+  height: 100%;
 }
 </style>
